bda051be33899c9ef60e3c04899ba9e55fbb4ad4
leomm <leommarques7@gmail.com>
2023-05-08 14:06:43 -0300

    test: add more get balance tests

--- domain/src/test/java/lmm/moneylog/domain/getbalance/fakerepos/FakeGetBalanceRepository1.kt
+++ domain/src/test/java/lmm/moneylog/domain/getbalance/fakerepos/FakeGetBalanceRepository1.kt
@@ -1,9 +1,10 @@
-package lmm.moneylog.domain.getbalance
+package lmm.moneylog.domain.getbalance.fakerepos
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import lmm.moneylog.domain.getbalance.GetBalanceRepository
 
-class FakeGetBalanceRepository: GetBalanceRepository {
+class FakeGetBalanceRepository1: GetBalanceRepository {
     override fun getAllTransactionsValues(): Flow<List<Double>> {
         return listOf(listOf(1.0, 2.0, 3.0)).asFlow()
     }
---
+++ domain/src/test/java/lmm/moneylog/domain/getbalance/fakerepos/FakeGetBalanceRepository4.kt
@@ -0,0 +1,11 @@
+package lmm.moneylog.domain.getbalance.fakerepos
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import lmm.moneylog.domain.getbalance.GetBalanceRepository
+
+class FakeGetBalanceRepository4: GetBalanceRepository {
+    override fun getAllTransactionsValues(): Flow<List<Double>> {
+        return listOf(emptyList<Double>()).asFlow()
+    }
+}
