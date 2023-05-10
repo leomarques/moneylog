366c151a352486f3f2716c0afdfdbf6923e4f6d9
leomm <leommarques7@gmail.com>
2023-05-10 09:02:33 -0300

    build: update compose to 2023.05.00

--- app/src/main/java/lmm/moneylog/ui/features/home/HomeViewModel.kt
+++ app/src/main/java/lmm/moneylog/ui/features/home/HomeViewModel.kt
@@ -1,15 +1,15 @@
 package lmm.moneylog.ui.features.home
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.map
 import lmm.moneylog.domain.getbalance.GetBalanceInteractor
 
 class HomeViewModel(interactor: GetBalanceInteractor) : ViewModel() {
     private val balanceData = interactor.execute().asLiveData()
 
-    val balanceCardModel: LiveData<BalanceCardModel> = Transformations.map(balanceData) { balance ->
+    val balanceCardModel: LiveData<BalanceCardModel> = balanceData.map { balance ->
         BalanceCardModel(
             formatValue(balance.total),
             formatValue(balance.credit),
