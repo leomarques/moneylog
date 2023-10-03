e7b6f4abe931b0163c1235dde90084ee4777777b
Leonardo Marques <leommarques7@gmail.com>
2023-10-03 19:13:23 -0300

    feat: add set_null to ondelete foreign keys

--- data/src/main/java/lmm/moneylog/data/transaction/database/TransactionEntity.kt
+++ data/src/main/java/lmm/moneylog/data/transaction/database/TransactionEntity.kt
@@ -2,7 +2,7 @@ package lmm.moneylog.data.transaction.database
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
-import androidx.room.ForeignKey.Companion.CASCADE
+import androidx.room.ForeignKey.Companion.SET_NULL
 import androidx.room.PrimaryKey
 import lmm.moneylog.data.account.database.AccountEntity
 import lmm.moneylog.data.category.database.CategoryEntity
@@ -14,13 +14,13 @@ import lmm.moneylog.data.category.database.CategoryEntity
             entity = AccountEntity::class,
             childColumns = ["accountId"],
             parentColumns = ["id"],
-            onDelete = CASCADE
+            onDelete = SET_NULL
         ),
         ForeignKey(
             entity = CategoryEntity::class,
             childColumns = ["categoryId"],
             parentColumns = ["id"],
-            onDelete = CASCADE
+            onDelete = SET_NULL
         )
     ]
 )
