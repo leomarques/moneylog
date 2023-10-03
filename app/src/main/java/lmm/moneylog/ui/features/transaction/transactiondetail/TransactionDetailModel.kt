8c67586838b403039b837d0aebc8e09f70a8a641
Leonardo Marques <leommarques7@gmail.com>
2023-10-03 16:40:56 -0300

    refactor: move lines

--- app/src/main/java/lmm/moneylog/ui/features/transaction/transactiondetail/TransactionDetailModel.kt
+++ app/src/main/java/lmm/moneylog/ui/features/transaction/transactiondetail/TransactionDetailModel.kt
@@ -6,12 +6,12 @@ import lmm.moneylog.R
 import lmm.moneylog.data.transaction.time.DomainTime
 
 data class TransactionDetailModel(
+    val id: Int = -1,
+    val titleResourceId: Int = R.string.detailtransaction_topbar_title_add,
+    val isEdit: Boolean = false,
+    val displayDate: String = "",
+    val date: DomainTime = DomainTime(0, 0, 0),
     val value: MutableState<String> = mutableStateOf(""),
     val isIncome: MutableState<Boolean> = mutableStateOf(true),
-    val description: MutableState<String> = mutableStateOf(""),
-    val displayDate: String = "",
-    val isEdit: Boolean = false,
-    val titleResourceId: Int = R.string.detailtransaction_topbar_title_add,
-    val id: Int = -1,
-    val date: DomainTime = DomainTime(0, 0, 0)
+    val description: MutableState<String> = mutableStateOf("")
 )
