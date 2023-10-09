3303aadae67e39cf5ffdf5dae169052a7ab6c747
Leonardo Marques <leommarques7@gmail.com>
2023-10-08 23:18:04 -0300

    fix: account transfer

--- app/src/main/java/lmm/moneylog/ui/features/account/getaccounts/GetAccountsViewModel.kt
+++ app/src/main/java/lmm/moneylog/ui/features/account/getaccounts/GetAccountsViewModel.kt
@@ -2,59 +2,49 @@ package lmm.moneylog.ui.features.account.getaccounts
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import lmm.moneylog.data.account.repositories.GetAccountsRepository
 import lmm.moneylog.data.accounttransfer.repositories.AccountTransferRepository
 import lmm.moneylog.data.balance.GetBalanceByAccountInteractor
 
 class GetAccountsViewModel(
-    private val getAccountsRepository: GetAccountsRepository,
-    private val accountTransferRepository: AccountTransferRepository,
+    getAccountsRepository: GetAccountsRepository,
+    accountTransferRepository: AccountTransferRepository,
     private val getBalanceByAccountInteractor: GetBalanceByAccountInteractor
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(GetAccountsModel())
-    val uiState: StateFlow<GetAccountsModel> = _uiState.asStateFlow()
+    private val accountsFlow = getAccountsRepository.getAccounts()
+    private val transfersFlow = accountTransferRepository.getTransfers()
 
-    init {
-        viewModelScope.launch {
-            getAccountsRepository.getAccounts().collect { accounts ->
-                accountTransferRepository.getTransfers().collect { transfers ->
-                    mutableListOf<AccountModel>().let { list ->
-                        accounts.forEach { account ->
-                            var balance = getBalanceByAccountInteractor.execute(account.id)
+    var uiState = accountsFlow.combine(transfersFlow) { accounts, transfers ->
+        val list = mutableListOf<AccountModel>()
 
-                            transfers.filter {
-                                it.originAccountId == account.id
-                            }.forEach {
-                                balance -= it.value
-                            }
+        accounts.forEach { account ->
+            var balance = getBalanceByAccountInteractor.execute(account.id)
 
-                            transfers.filter {
-                                it.destinationAccountId == account.id
-                            }.forEach {
-                                balance += it.value
-                            }
-
-                            list.add(
-                                AccountModel(
-                                    id = account.id,
-                                    name = account.name,
-                                    balance = balance
-                                )
-                            )
-                        }.also {
-                            _uiState.update {
-                                GetAccountsModel(list)
-                            }
-                        }
-                    }
-                }
+            transfers.filter {
+                it.originAccountId == account.id
+            }.forEach {
+                balance -= it.value
             }
+
+            transfers.filter {
+                it.destinationAccountId == account.id
+            }.forEach {
+                balance += it.value
+            }
+
+            list.add(
+                AccountModel(
+                    id = account.id,
+                    name = account.name,
+                    balance = balance
+                )
+            )
         }
-    }
+
+        GetAccountsModel(list)
+    }.stateIn(viewModelScope, SharingStarted.Lazily, GetAccountsModel())
 }
