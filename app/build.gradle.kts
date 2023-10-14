bb09c1d758649c354a66f6d38b716e8d6a0f67b4
Leonardo Marques <leommarques7@gmail.com>
2023-10-14 07:20:33 -0300

    build: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2.0a"
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
