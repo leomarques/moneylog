810e3321d64fcafe8f4587fe09bdadcd40277a41
Leonardo Marques <leommarques7@gmail.com>
2024-04-24 07:45:20 -0300

    build: update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2.3b"
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
